bf475eee2418173dfda2231141286a7686a9eca8
Edmarc Sapa <marcgsapa@gmail.com>
2024-06-17 13:33:32 -0700

    start button fade

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,18 @@
 "use strict";
+
+// Start Screen
+const startBtn = document.querySelector(".start-btn");
+const infScrollBg = document.querySelector(".infinite-scroll");
+
+// Start Button Clicked
+
+startBtn.addEventListener("click", () => {
+  infScrollBg.classList.add("fast");
+  console.log("Start Button Clicked");
+  startBtn.classList.add("hide");
+  if (infScrollBg.classList.contains("fast")) {
+    setInterval(() => {
+      startBtn.style.display = "none";
+    }, 500);
+  }
+});
